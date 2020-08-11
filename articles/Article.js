aef14cb76fda6cf51282e11c6e3737280a0ee377
Clebson Santos <clebsonsantosmtt@gmail.com>
2020-08-11 13:54:05 -0300

    first commit

---
+++ articles/Article.js
@@ -0,0 +1,24 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category");
+
+const Article = connection.define('articles',{
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }, slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    body: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    }
+})
+
+Category.hasMany(Article); // Tem muitos artigos -- hasmany(tem muitos)
+Article.belongsTo(Category); // um artigo ´ pertence a´  uma categoria -- relacionamento entre artigo e categoria
+
+// Article.sync({force: true});
+
+module.exports = Article;
