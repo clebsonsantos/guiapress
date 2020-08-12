ed793a2057cbcfc8325612f9a003d83e3d08314b
Clebson Santos <clebsonsantosmtt@gmail.com>
2020-08-12 16:56:56 -0300

    adição de artigos

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -25,7 +25,7 @@ router.post("/categories/save", (req, res)=>{
 });
 router.get("/admin/categories", (req, res) =>{
     Category.findAll().then(categories => {
-        res.render("/admin/categories/index", {categories: categories});
+        res.render("admin/categories/index", {categories: categories});
     });
 });
 
@@ -59,7 +59,7 @@ router.post("/categories/delete", (req, res) => {
     }
     Category.findByPk(id).then(category =>{
         if(category != undefined){
-            res.render("/admin/categories/edit", { category: category});
+            res.render("admin/categories/edit", { category: category});
         }else{
             res.redirect("/admin/categories");
         }
